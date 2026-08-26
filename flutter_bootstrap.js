bd3e955ce42e18440a71c846d92f28a5b7ef04e9
pieringro <pieringro@users.noreply.github.com>
2026-08-26 11:50:25 +0000

    deploy: pieringro/idle-game1@b3bae0e2c48a97638e22681fc6e081b32105d9fe

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "410739365" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2805486185" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
